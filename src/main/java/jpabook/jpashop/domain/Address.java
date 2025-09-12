36904e0fef576d135ed6743839c3b81bdbd35e36
hyeonguk <shyunguk126@naver.com>
2025-09-13 02:59:14 +0900

    feat: 도메인 모델 및 연관관계 매핑 추가

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
+    // jpa 스펙상 기본 생성자 필요함
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
